0802fc3697b7bc5a10370d7b76adec62bd3c6554
healim01 <22000770@handong.edu>
2023-06-04 21:32:11 +0900

    fix: change route

---
+++ lib/screen/home.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffFF7C33),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(height: 60.0),
+                Image.asset(
+                  'assets/logo.png',
+                  width: 300,
+                ),
+                const Center(
+                  child: Text(
+                    'MEALMATE',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'Inter',
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 400,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/ingredient');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'Ingredient-based recipe',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontFamily: 'Inter',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 400,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/me');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'Menu-based recipes',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontFamily: 'Inter',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 400,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'Meal Recommendation',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontFamily: 'Inter',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
