e8869723f06989591521b465e6e472fda3c3d837
healim01 <22000770@handong.edu>
2023-06-04 20:10:27 +0900

    fix: remove unused

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mealmate/app.dart';
 
-import 'package:mealmate/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
