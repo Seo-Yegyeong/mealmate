74df85438b37a2e099ab6df1f1339458203ca8f5
Lab <seoye6618@gmail.com>
2023-06-04 20:51:26 +0900

    [FEAT] test for GPT API

---
+++ lib/test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const apiKey = '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';
+const apiUrl = 'https://api.openai.com/v1/completions';
