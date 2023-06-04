6dd9a3b6d824315b67dda3357e9bf68bad7fec08
healim01 <22000770@handong.edu>
2023-06-04 22:08:18 +0900

    feat: add route

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:mealmate/screen/chat_screen.dart';
-import 'package:mealmate/screen/home.dart';
 import 'package:mealmate/screen/ingredient.dart';
+import 'package:mealmate/screen/home.dart';
+import 'package:mealmate/screen/meal_recommend.dart';
+import 'package:mealmate/screen/menu_based.dart';
 
-// import 'package:mealmate/screen/new_message.dart';
 import 'package:mealmate/screen/start.dart';
 
 class MealMateAPP extends StatelessWidget {
@@ -17,8 +17,9 @@ class MealMateAPP extends StatelessWidget {
       routes: {
         '/home': (BuildContext context) => const HomePage(),
         '/ingredient': (BuildContext context) => const IngredientPage(),
+        '/menubased': (BuildContext context) => const MenuBasedPage(),
+        '/mealrecommend': (BuildContext context) => const MealRecommendPage(),
         '/': (BuildContext context) => const StartPage(),
-        '/me': (BuildContext context) => const ChatScreen(),
       },
       theme: ThemeData.light(useMaterial3: true),
     );
