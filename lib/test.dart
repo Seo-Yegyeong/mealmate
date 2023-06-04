5cf417aa7143b08f46e822cf47822b15022f0626
Lab <seoye6618@gmail.com>
2023-06-04 22:31:34 +0900

    remove the test.dart

--- lib/test.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-const apiKey = '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';
-const apiUrl = 'https://api.openai.com/v1/completions';
